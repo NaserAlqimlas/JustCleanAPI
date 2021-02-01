556b2cad89528e58052495a03bb7b12784738045
NaserAlqimlas <naser.alqimlas@gmail.com>
2021-02-01 17:37:24 +0300

    using middleware for jwt

--- routes/towers.js
+++ routes/towers.js
@@ -52,7 +52,14 @@ router.get("/get", (req, res) => {
         res.status(200).json(towers);
       })
       .catch((err) => res.status(404).json({ error: err }));
-  } else if (req.query.name || req.query.location || req.query.numberOfFloors || req.query.rating || req.query.longitude || req.query.latitude) {
+  } else if (
+    req.query.name ||
+    req.query.location ||
+    req.query.numberOfFloors ||
+    req.query.rating ||
+    req.query.longitude ||
+    req.query.latitude
+  ) {
     // filter/search by passing what to search by (col) and the search query
     const filterquery = req.query.filterquery;
     const filtercol = req.query.filtercol;
@@ -63,25 +70,30 @@ router.get("/get", (req, res) => {
       limit: req.query.limit,
       offset: req.query.offset,
       where: {
-        [Op.or]: [{
-          name: {[Op.iLike]: `${req.query.name || ""}`}
-        },
-        {
-          location: {[Op.iLike]: `${req.query.location || ""}`}
-        },
-        {
-          numberOfFloors: {[Op.iLike]: Number(`${req.query.numberOfFloors || 0}`)}
-        },
-        {
-          rating: {[Op.iLike]: parseFloat(`${req.query.rating || 0.0}`)}
-        },
-        {
-          longitude: {[Op.iLike]: parseFloat(`${req.query.longitude || 0.0}`)}
-        },
-        {
-          latitude: {[Op.iLike]: parseFloat(`${req.query.latitude || 0.0}`)}
-        },
-      ]
+        [Op.or]: [
+          {
+            name: { [Op.iLike]: `${req.query.name || ""}` }
+          },
+          {
+            location: { [Op.iLike]: `${req.query.location || ""}` }
+          },
+          {
+            numberOfFloors: {
+              [Op.iLike]: Number(`${req.query.numberOfFloors || 0}`)
+            }
+          },
+          {
+            rating: { [Op.iLike]: parseFloat(`${req.query.rating || 0.0}`) }
+          },
+          {
+            longitude: {
+              [Op.iLike]: parseFloat(`${req.query.longitude || 0.0}`)
+            }
+          },
+          {
+            latitude: { [Op.iLike]: parseFloat(`${req.query.latitude || 0.0}`) }
+          }
+        ]
       }
     })
       .then((towers) => {
@@ -98,23 +110,37 @@ router.get("/get", (req, res) => {
 });
 
 // Create a new tower entry
-router.post("/create", verifyToken, (req, res) => {
-  jwt.verify(req.token, "supersecretkey", (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
+// router.post("/create", verifyToken, (req, res) => {
+//   jwt.verify(req.token, "supersecretkey", (err, authData) => {
+//     if (err) {
+//       res.sendStatus(403);
+//     } else {
+//       Tower.create(req.body)
+//         .then((tower) => res.status(200).json(tower))
+//         .catch((err) => res.status(500).json({ error: err }));
+//     }
+//   });
+// });
+
+router.post("/create", verifyToken, (req, res, next) => {
+  jwt.verify(
+    req.token,
+    "supersecretkey",
+    (err, authData) => (err ? res.sendStatus(403) : next()),
+    () => {
       Tower.create(req.body)
         .then((tower) => res.status(200).json(tower))
         .catch((err) => res.status(500).json({ error: err }));
     }
-  });
+  );
 });
 
-router.put("/update", verifyToken, async (req, res) => {
-  jwt.verify(req.token, "supersecretkey", async (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
+router.put("/update", verifyToken, async (req, res, next) => {
+  jwt.verify(
+    req.token,
+    "supersecretkey",
+    (err, authData) => (err ? res.sendStatus(403) : next()),
+    () => {
       const { id } = req.body;
       // delete id so we pass req.body in tower.update without running into issues
       delete req.body.id;
@@ -123,19 +149,20 @@ router.put("/update", verifyToken, async (req, res) => {
         .then((rowsUpdated) => res.sendStatus(200).json(rowsUpdated))
         .catch((err) => res.status(404).json({ error: err, id: id }));
     }
-  });
+  );
 });
 
-router.delete("/delete", verifyToken, (req, res) => {
-  jwt.verify(req.token, "supersecretkey", (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
+router.delete("/delete", verifyToken, (req, res, next) => {
+  jwt.verify(
+    req.token,
+    "supersecretkey",
+    (err, authData) => (err ? res.sendStatus(403) : next()),
+    () => {
       Tower.destroy({ where: { id: req.query.id } })
         .then((rowsDeleted) => res.sendStatus(200).json(rowsDeleted))
         .catch((err) => res.status(404).json({ error: err, id: req.query.id }));
     }
-  });
+  );
 });
 
 module.exports = router;
